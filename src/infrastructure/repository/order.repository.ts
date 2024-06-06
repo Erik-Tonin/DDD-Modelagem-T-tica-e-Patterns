1d4a7de5fb5e25f125d56ad733d9ee49cfaf7cae
Erik-Tonin <eriktonin.dev@gmail.com>
2024-06-05 21:32:56 -0300

    Finalizando Order repository

--- src/infrastructure/repository/order.repository.ts
+++ src/infrastructure/repository/order.repository.ts
@@ -1,7 +1,7 @@
 import Order from "../../domain/entity/order";
+import OrderItem from "../../domain/entity/order_item";
 import OrderItemModel from "../db/sequelize/model/order-item.model";
 import OrderModel from "../db/sequelize/model/order.model";
-
 
 export default class OrderRepository {
   async create(entity: Order): Promise<void> {
@@ -44,23 +44,43 @@ export default class OrderRepository {
       }
     );
   }
-
+  
   async find(id: string): Promise<Order> {
     let orderModel;
-
-    try{
+  
+    try {
       orderModel = await OrderModel.findOne({
-        where:{
+        where: {
           id,
         },
+        include: [{ model: OrderItemModel }],
         rejectOnEmpty: true,
       });
+    } catch (error) {
+      throw new Error("Order not found");
     }
-      catch (error){
-        throw new Error("Order not found");
-    }
-
-    const order = new Order(orderModel.id, orderModel.customer_id, [])
+  
+    const orderItems = orderModel.items.map(item => 
+      new OrderItem(item.id, item.name, item.price, item.product_id, item.quantity)
+    );
+  
+    const order = new Order(orderModel.id, orderModel.customer_id, orderItems);
+  
     return order;
   }
+
+  async findAll(): Promise<Order[]> {
+    const orderModels = await OrderModel.findAll({
+      include: [{ model: OrderItemModel }],
+    });
+  
+    return orderModels.map((orderModel) => {
+      const orderItems = orderModel.items.map(item =>
+        new OrderItem(item.id, item.name, item.price, item.product_id, item.quantity)
+      );
+      
+      return new Order(orderModel.id, orderModel.customer_id, orderItems);
+    });
+  }
+
 }
